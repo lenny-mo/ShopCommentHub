bf4c14c847acda6238baca4a7205ebaf00412100
Lenny Mo <lennymo.work@gmail.com>
2024-01-15 19:06:38 +0800

    add validator middleware

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/validate"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
 )
 
@@ -15,6 +16,7 @@ func NewGRPCServer(c *conf.Server, commentservice *service.CommentServiceService
 	var opts = []grpc.ServerOption{
 		grpc.Middleware(
 			recovery.Recovery(),
+			validate.Validator(), //添加参数校验中间件
 		),
 	}
 	if c.Grpc.Network != "" {
--- internal/server/http.go
+++ internal/server/http.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/validate"
 	"github.com/go-kratos/kratos/v2/transport/http"
 )
 
@@ -15,6 +16,7 @@ func NewHTTPServer(c *conf.Server, commentservice *service.CommentServiceService
 	var opts = []http.ServerOption{
 		http.Middleware(
 			recovery.Recovery(),
+			validate.Validator(), //添加参数校验中间件
 		),
 	}
 	if c.Http.Network != "" {
