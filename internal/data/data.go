f41ec1c9bb653dd65c1ef597bebd6d20a732aa81
Lenny Mo <lennymo.work@gmail.com>
2024-01-16 14:13:58 +0800

    add mongo db for eventbus

--- internal/data/data.go
+++ internal/data/data.go
@@ -3,31 +3,35 @@ package data
 import (
 	"comment/internal/conf"
 	"comment/internal/data/query"
+	"context"
 
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/google/wire"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 // ProviderSet is data providers.
-var ProviderSet = wire.NewSet(NewData, NewDB, NewCustomerRepo, NewMerchantRepo)
+var ProviderSet = wire.NewSet(NewData, NewDB, NewMongo, NewCustomerRepo, NewMerchantRepo, NewMongoRepo)
 
 // Data .
 type Data struct {
 	//  wrapped database client
-	q   *query.Query
-	log *log.Helper
+	q     *query.Query    // mysql
+	mongo *mongo.Database // mongo
+	log   *log.Helper
 }
 
 // NewData .
 // 修改：添加gorm.DB
-func NewData(db *gorm.DB, logger log.Logger) (*Data, func(), error) {
+func NewData(db *gorm.DB, mongo *mongo.Database, logger log.Logger) (*Data, func(), error) {
 	cleanup := func() {
 		log.NewHelper(logger).Info("closing the data resources")
 	}
 	query.SetDefault(db) // 选择数据库
-	return &Data{q: query.Q, log: log.NewHelper(logger)}, cleanup, nil
+	return &Data{q: query.Q, mongo: mongo, log: log.NewHelper(logger)}, cleanup, nil
 }
 
 func NewDB(cfg *conf.Data) *gorm.DB {
@@ -42,4 +46,20 @@ func NewDB(cfg *conf.Data) *gorm.DB {
 	}
 
 	return nil
+}
+
+func NewMongo(cfg *conf.Data) *mongo.Database {
+	// 设置连接选项
+	clientOptions := options.Client().ApplyURI(cfg.Mongo.Source)
+
+	// 连接到 MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		return nil
+	}
+
+	// 连接到指定数据库
+	db := client.Database(cfg.Mongo.Db)
+
+	return db
 }
---
+++ internal/data/mongo.go
@@ -0,0 +1,27 @@
+package data
+
+import (
+	"comment/internal/biz"
+	"context"
+
+	"github.com/go-kratos/kratos/v2/log"
+)
+
+type MongoRepo struct {
+	data *Data
+	log  *log.Helper
+}
+
+// NewCustomerRepo
+func NewMongoRepo(data *Data, logger log.Logger) biz.CustomerRepo {
+	return &CustomerRepo{
+		data: data,
+		log:  log.NewHelper(logger),
+	}
+}
+
+// Insert 向mongo数据库中的指定表插入一条数据
+func (m *MongoRepo) Insert(ctx context.Context, colle string, data interface{}) (interface{}, error) {
+	m.data.mongo.Collection(colle).InsertOne(ctx, data)
+	return data, nil
+}
