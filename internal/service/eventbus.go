4249d442eb7694fc8f5b7be15dce8bd073d180d7
Lenny Mo <lennymo.work@gmail.com>
2024-01-22 16:22:12 +0800

    add cron task to handle unsuccessfully sent msg

--- internal/service/eventbus.go
+++ internal/service/eventbus.go
@@ -21,6 +21,28 @@ func InitEventBus() {
 	// 监听事件
 	general_bus.Subscribe("customer:AddComment:success", help)
 	general_bus.Subscribe("customer:AddComment:fail", help)
+	general_bus.Subscribe("customer:AddComment:success", sendToMQ)
+	general_bus.Subscribe("customer:AddComment:fail", printFail)
+	general_bus.Subscribe("scanMongoSendUnsuccessMsg", scanUserCommentMongo)
+	c = cron.NewCron(time.Second, 100)
+	c.Run()
+	// 创建task
+	var f func(ctx context.Context) = func(ctx context.Context) {
+		once := new(sync.Once)
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				// 向事件总线发送事件,扫描mongo中的sentMQ字段, 如果为false 则拿出来重新发送
+				once.Do(func() {
+					general_bus.Publish("scanMongoSendUnsuccessMsg")
+				})
+			}
+		}
+	}
+	// 添加定时任务
+	c.AddTask(*cron.NewTask(f, 5*time.Second))
 }
 
 // 接收事件参数 发现事件之后写入mongodb以及 kafka
