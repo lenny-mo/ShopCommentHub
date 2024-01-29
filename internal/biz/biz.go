621d8fc75af728edc8798f7d373a5610e45cc6da
Lenny Mo <lennymo.work@gmail.com>
2024-01-29 18:07:56 +0800

    biz update

--- internal/biz/biz.go
+++ internal/biz/biz.go
@@ -3,4 +3,4 @@ package biz
 import "github.com/google/wire"
 
 // ProviderSet is biz providers.
-var ProviderSet = wire.NewSet(NewCustomerUsecase, NewMerchantUsecase)
+var ProviderSet = wire.NewSet(NewCustomerUsecase, NewMerchantUsecase, NewBusUseCase, NewReadModelUsecase)
